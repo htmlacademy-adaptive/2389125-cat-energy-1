237b020daad6894ed9039bafa504be0b186502da
Dmitriy Uzhakhov <alhimik.web@yandex.ru>
2024-09-23 14:50:47 +0300

    Добавляет Оптимизацию картинок,Webp и мелкие исправления

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,10 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import imagemin from 'gulp-imagemin';
+import webp from 'gulp-webp';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
 
@@ -12,11 +16,30 @@ export const styles = () => {
     .pipe(plumber())
     .pipe(sass().on('error', sass.logError))
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' }))
     .pipe(browser.stream());
 }
+
+//Images
+
+export const images = () => {
+return gulp.src('source/img/**/*.{jpg,png,svg}')
+.pipe(imagemin())
+.pipe(gulp.dest('build/img'))
+}
+
+//Webp
+
+export const createWebp = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+  .pipe(webp())
+  .pipe(gulp.dest('build/img'))
+}
+
 
 // Server
 
